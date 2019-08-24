2bd3178b003c5cbf7032a822702f3b3fa6dfc6d8
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-08-24 20:47:43 +0530

    printing speed and distance

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -10,7 +10,16 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
-		
+		Scanner scanner=new Scanner(System.in);
+		double speed;
+		double distance;
+		double gravity=9.8;
+		double t=scanner.nextDouble();
+		speed=gravity*t;
+		distance=0.5*gravity*t*t;
+        System.out.println("The speed of the object at "+t+" seconds after its release is "+speed+" and the distance the object has travelled in the "+t+" seconds after the relase is "+distance+".");
+        scanner.close();
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
