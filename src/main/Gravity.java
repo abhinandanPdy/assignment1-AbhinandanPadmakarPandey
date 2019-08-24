2a255b4e996a46660da88a140b4c2d4e639512eb
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-08-24 20:22:07 +0530

    decaring variables

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -1,11 +1,6 @@
-/*
- * @author Divyansh Bhardwaj
- * @version 1.0
- * */
 package main;
 
-// import java.util.Scanner;
-// uncomment the above line to use input in your program.
+import java.util.Scanner;
 
 public class Gravity {
 	// TODO 1: Create the main method
@@ -15,6 +10,7 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
+		
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
